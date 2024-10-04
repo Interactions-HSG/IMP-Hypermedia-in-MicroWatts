686024e14c800d459903f969ca4cedc2ffbff3f2
Kai <112092835+KaiTries@users.noreply.github.com>
2024-10-04 21:56:19 +0200

    cleaned ups sensingAgent a bit

--- src/SensingAgent/simulated/app/src/main/java/org/example/SensingAgent.java
+++ src/SensingAgent/simulated/app/src/main/java/org/example/SensingAgent.java
@@ -5,24 +5,30 @@ import static org.eclipse.rdf4j.model.util.Values.iri;
 import ch.unisg.ics.interactions.wot.td.ThingDescription;
 import ch.unisg.ics.interactions.wot.td.io.TDGraphReader;
 import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapServer;
 import org.eclipse.californium.core.coap.CoAP;
 import org.eclipse.californium.core.coap.Option;
 import org.eclipse.californium.core.coap.OptionSet;
 import org.eclipse.californium.core.coap.Request;
+import org.eclipse.californium.core.server.resources.Resource;
 import org.eclipse.californium.elements.exception.ConnectorException;
 
 public class SensingAgent extends CoapServer {
 
   private final String ENTRYPOINT;
   private final String metadata;
-  private String location;
+  private final String location;
   private String locationUri;
   private String baseUriYggdrasil;
   private String dbPostSensorDataUri;
 
   private boolean running = false;
+  private ScheduledExecutorService scheduler;
 
   public SensingAgent(String entrypoint, String metadata, String room) {
     super(5684);
@@ -32,21 +38,17 @@ public class SensingAgent extends CoapServer {
     this.locationUri = null;
   }
 
+  @Override
+  protected Resource createRoot() {
+    return new SensingAgentRootResource();
+  }
+
   public void run() throws ConnectorException, IOException {
     this.start();
     this.setup();
     running = true;
-    while (running) {
-      System.out.println("Sensing...");
-      sendSensingData();
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
-    this.stop();
-    this.destroy();
+    scheduler = Executors.newScheduledThreadPool(1);
+    scheduler.scheduleAtFixedRate(this::sendSensingData, 0, 10, TimeUnit.SECONDS);
   }
 
   /**
@@ -58,17 +60,27 @@ public class SensingAgent extends CoapServer {
    */
   private void setup() throws ConnectorException, IOException {
     System.out.println("Setting up...");
-    final String platformRepresentation = sendCoapMessage(ENTRYPOINT);
+    String platformRepresentation = sendCoapMessage(ENTRYPOINT);
+    while(platformRepresentation == null) {
+      System.out.println("Failed to get platform representation");
+      platformRepresentation = sendCoapMessage(ENTRYPOINT);
+      try {
+        Thread.sleep(10000);
+      } catch (InterruptedException e) {
+        System.err.println("Failed to sleep");
+      }
+    }
     final var td = TDGraphReader.
         readFromString(ThingDescription.TDFormat.RDF_TURTLE, platformRepresentation);
     baseUriYggdrasil = cleanUri(td.getThingURI().orElseThrow());
+
     var joined = joinMyWorkspace();
     while (!joined) {
       System.out.println("Failed to join workspace");
       try {
         Thread.sleep(10000);
       } catch (InterruptedException e) {
-        e.printStackTrace();
+        System.err.println("Failed to sleep");
       }
       joined = joinMyWorkspace();
     }
@@ -80,7 +92,7 @@ public class SensingAgent extends CoapServer {
       try {
         Thread.sleep(10000);
       } catch (InterruptedException e) {
-        e.printStackTrace();
+        System.err.println("Failed to sleep");
       }
       dbFound = findDB();
     }
@@ -89,6 +101,11 @@ public class SensingAgent extends CoapServer {
 
   public void stopRunning() {
     running = false;
+    if (scheduler != null) {
+      scheduler.shutdown();
+    }
+    this.stop();
+    this.destroy();
   }
 
   private String sendCoapMessage(final String targetUri) throws ConnectorException, IOException {
@@ -119,6 +136,10 @@ public class SensingAgent extends CoapServer {
       return false;
     }
     final String DBRepresentation = sendCoapMessage(DBArtifactUri);
+    if (DBRepresentation == null) {
+      System.out.println("DB representation not found");
+      return false;
+    }
     final var dbTD = TDGraphReader
         .readFromString(ThingDescription.TDFormat.RDF_TURTLE, DBRepresentation);
 
@@ -129,19 +150,34 @@ public class SensingAgent extends CoapServer {
       return false;
     }
 
+    if (dbEndpoint.get().getFirstForm().isEmpty()) {
+      System.out.println("DB endpoint form not found");
+      return false;
+    }
     dbPostSensorDataUri = dbEndpoint.get().getFirstForm().get().getTarget();
     System.out.println("DB endpoint: " + dbEndpoint.get().getFirstForm().get().getTarget());
     return true;
   }
 
-  private void sendSensingData() throws ConnectorException, IOException {
+  private void sendSensingData() {
+    if(!running) {
+      return;
+    }
+    try {
     System.out.println("Sending sensing data...");
     final var coapClient = new CoapClient(dbPostSensorDataUri);
     Request request = new Request(CoAP.Code.POST, CoAP.Type.NON);
-    request.setPayload("\"temperature\":\"20\",\"humidity\":\"50\"");
+    final var randomTemperature = new Random().nextInt(30);
+    final var randomHumidity = new Random().nextInt(100);
+    request.setPayload("\"temperature\":\" "
+        + randomTemperature
+        + "\",\"humidity\":\""
+        + randomHumidity + "\"");
     coapClient.advanced(request); // or async version
     coapClient.shutdown();
-
+    } catch (Exception e) {
+      System.err.println("Failed to send sensing data: " + e.getMessage());
+    }
   }
 
   private boolean joinMyWorkspace() throws ConnectorException, IOException {
@@ -181,12 +217,19 @@ public class SensingAgent extends CoapServer {
   }
 
   private String getJoinFormUri(String workspaceName, String WorkspaceRepresentation) {
+    if (WorkspaceRepresentation == null) {
+      return null;
+    }
     ThingDescription td = TDGraphReader
         .readFromString(ThingDescription.TDFormat.RDF_TURTLE, WorkspaceRepresentation);
     final var joinWorkspaceAffordance = td.getActionByName("joinWorkspace");
     if (joinWorkspaceAffordance.isPresent()) {
       final var joinWorkspaceAction = joinWorkspaceAffordance.get();
       final var joinWorkspaceForm = joinWorkspaceAction.getFirstForm();
+        if (joinWorkspaceForm.isEmpty()) {
+            System.out.println("Join form not found");
+            return null;
+        }
       System.out.println("Join form found: " + joinWorkspaceForm.get().getTarget());
       return ENTRYPOINT + "workspaces/" + workspaceName + "/join";
     }
@@ -209,6 +252,9 @@ public class SensingAgent extends CoapServer {
   public String getArtifactUri(String artifactName, String workspaceRepresenation) {
     ThingDescription td =
         TDGraphReader.readFromString(ThingDescription.TDFormat.RDF_TURTLE, workspaceRepresenation);
+    if (td == null || td.getThingURI().isEmpty()) {
+      return null;
+    }
     final var workspaceUri = cleanUri(td.getThingURI().get());
     final var expectedArtifactURI = workspaceUri + "/artifacts/" + artifactName + "/#artifact";
     final var foundArtifact = findArtifactUri(td, expectedArtifactURI);
@@ -220,17 +266,23 @@ public class SensingAgent extends CoapServer {
   }
 
   public boolean findArtifactUri(ThingDescription td, final String expectedArtifactURI) {
+    if (td == null || td.getGraph().isEmpty()) {
+      return false;
+    }
     final var model = td.getGraph().get();
     final var t =
         model.filter(null, iri("https://purl.org/hmas/contains"), iri(expectedArtifactURI));
-    return t.size() > 0;
+    return !t.isEmpty();
   }
 
 
   public boolean findWorkspaceUri(ThingDescription td, final String expectedWorkspaceURI) {
+    if (td == null || td.getGraph().isEmpty()) {
+      return false;
+    }
     final var model = td.getGraph().get();
     final var t = model.filter(null, iri("https://purl.org/hmas/hosts"), iri(expectedWorkspaceURI));
-    return t.size() > 0;
+    return !t.isEmpty();
   }
 
   private String cleanUri(final String ucleanUri) {

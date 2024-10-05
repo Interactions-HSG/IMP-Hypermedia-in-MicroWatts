6a52deba875f8d3c2176195c58840a738071cbe1
nikokelx <nikolaj.keller@protonmail.com>
2024-10-05 15:08:36 +0200

    configure yggdrasil

--- src/OrgManager/src/main/java/ch/unisg/orgmanager/config/YggdrasilConfig.java
+++ src/OrgManager/src/main/java/ch/unisg/orgmanager/config/YggdrasilConfig.java
@@ -1,6 +1,11 @@
 package ch.unisg.orgmanager.config;
 
 import ch.unisg.ics.interactions.wot.td.ThingDescription;
+import ch.unisg.ics.interactions.wot.td.affordances.ActionAffordance;
+import ch.unisg.ics.interactions.wot.td.io.TDGraphReader;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class YggdrasilConfig {
 
@@ -8,8 +13,15 @@ public class YggdrasilConfig {
     public static String WORKSPACE = "root";
     public static String NAME = "OrgManager";
 
-
     public YggdrasilConfig() {
+        try {
+            ThingDescription tdYggdrasil = TDGraphReader.readFromURL(ThingDescription.TDFormat.RDF_TURTLE, ENTRYPOINT);
+
+            ThingDescription tdWorkspace = TDGraphReader.readFromURL(ThingDescription.TDFormat.RDF_TURTLE, ENTRYPOINT + "workspaces/" + WORKSPACE);
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
 
     }
 }

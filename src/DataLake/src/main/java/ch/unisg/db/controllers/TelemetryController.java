c129728a86cb40b070189bb36edf7df1605776ee
nikokelx <nikolaj.keller@protonmail.com>
2024-10-07 19:08:34 +0200

    update

--- src/DataLake/src/main/java/ch/unisg/db/controllers/TelemetryController.java
+++ src/DataLake/src/main/java/ch/unisg/db/controllers/TelemetryController.java
@@ -3,11 +3,15 @@ package ch.unisg.db.controllers;
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 public class TelemetryController extends CoapResource {
 
-    List<String> telemetryData;
+    HashMap<String, Object> telemetries = new HashMap<>();
 
     public TelemetryController(String uri) {
         super(uri);
@@ -17,14 +21,19 @@ public class TelemetryController extends CoapResource {
 
     @Override
     public void handleGET(CoapExchange exchange) {
-        exchange.respond(telemetryData.toString());
+
+        exchange.respond(telemetries.toString());
     }
 
     @Override
     public void handlePOST(CoapExchange exchange) {
         exchange.accept();
         final var payload = exchange.getRequestText();
-        telemetryData.add(payload);
+
+        Date test = new Timestamp(System.currentTimeMillis());
+
+        telemetries.put(test.toString(), payload);
+
         System.out.println("Received payload: " + payload);
         exchange.respond("Received payload.");
     }
